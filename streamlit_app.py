b40b197316a747e49f4411831f62af4763ebbc90
Benjamin Mellon <97412534+Cantaloupe2@users.noreply.github.com>
2023-10-26 08:58:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ df["year"] = df["versions"].str[-20:-16]
 
 add_sidebar = st.sidebar.selectbox("Select Display",("Barchart","Exponential Fit"))
 # make a barplot of the year column based on freqeuncy of each year sorted by year
-st.bar_chart(df["year"].value_counts().sort_index())
+if add_sidebar == "Barchart":
+    st.bar_chart(df["year"].value_counts().sort_index())
 # display the above plot in the streamlit app
 
 
@@ -18,6 +19,8 @@ st.bar_chart(df["year"].value_counts().sort_index())
 from scipy.optimize import curve_fit
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 def func(x, a,b):
     return b*2**(a*(x-1992))
 
@@ -31,21 +34,22 @@ papers_per_year.index = papers_per_year.index.astype(int)
 
 popt, pcov = curve_fit(func, papers_per_year.index[:-1], papers_per_year[:-1])
 
-# plot papers per year and the fitted function
-plt.scatter(papers_per_year.index[:-1], papers_per_year[:-1], label="y = b*2^(a(x-2000))"+"\na = " + str(popt[0]) + "\nb = " + str(popt[1]))
-plt.plot(papers_per_year.index[:-1], func(papers_per_year.index[:-1], *popt), 'r-', label='Fit')
-plt.scatter(papers_per_year.index[:-1],papers_per_year[:-1],color = "b", label = "Data")
-# add text to the legend of the graph
-
-print("y = b*2^(a(x-1992))")
-print("a = " + str(popt[0]))
-print("b = " + str(popt[1]))
-
-# add appropriate labels
-plt.xlabel('Year')
-plt.ylabel("Number of Papers Per Year")
-plt.title("Doubling Time is "+ str(1/popt[0]) + "years")
-# html figure
-plt.legend()
-st.pyplot()
-#plt.savefig('doubling.png')
+if add_sidebar == "Exponential Fit":
+    # plot papers per year and the fitted function
+    plt.scatter(papers_per_year.index[:-1], papers_per_year[:-1], label="y = b*2^(a(x-2000))"+"\na = " + str(popt[0]) + "\nb = " + str(popt[1]))
+    plt.plot(papers_per_year.index[:-1], func(papers_per_year.index[:-1], *popt), 'r-', label='Fit')
+    plt.scatter(papers_per_year.index[:-1],papers_per_year[:-1],color = "b", label = "Data")
+    # add text to the legend of the graph
+    
+    print("y = b*2^(a(x-1992))")
+    print("a = " + str(popt[0]))
+    print("b = " + str(popt[1]))
+    
+    # add appropriate labels
+    plt.xlabel('Year')
+    plt.ylabel("Number of Papers Per Year")
+    plt.title("Doubling Time is "+ str(1/popt[0]) + "years")
+    # html figure
+    plt.legend()
+    st.pyplot()
+    #plt.savefig('doubling.png')
