89fdf864f48ca47295f2f712c34450b8c199bbc9
Ben Michael Mellon <mellonbe@moria.egr.msu.edu>
2023-11-02 08:12:40 -0400

    ...

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("Doubling Time Visualization")
 
 
 df = pd.read_csv("reduced.csv")
-if st.button("Show Data"):
+if st.toggle("Show Data"):
     st.dataframe(df.head(20))
 
 df["year"] = df["versions"].str[-20:-16]
