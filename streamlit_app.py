1e0778069627cd7d1461c4b631bc79f5449c995a
Ben Michael Mellon <mellonbe@moria.egr.msu.edu>
2023-11-02 09:16:36 -0400

    ...

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,6 +63,6 @@ if add_sidebar == "Top Authors":
     top_authors = df["submitter"].value_counts().sort_values(ascending=False).head(20)
     # sort top_authors by number of papers submitted
     top_authors_index = sorted(top_authors)
-    top_authors = top_authors[top_authors_index.index()]
-    st.write(top_authors)
+    # st.write(top_authors)
+    
     st.bar_chart(top_authors)
