72e48c3b3eb65824b05ec5043201e054b1c869f6
Ben Michael Mellon <mellonbe@moria.egr.msu.edu>
2023-11-08 10:35:53 -0500

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,5 +103,5 @@ if add_sidebar == "Abstract Lengths":
 if add_sidebar == "Paper Recommender":
     power_search = st.text_input("Search Papers by Title")
     tfidf_vectorizer = TfidfVectorizer(stop_words='english') 
-    tfidf_vector = tfidf_vectorizer.fit_transform(df["abstract"][0])
+    tfidf_vector = tfidf_vectorizer.fit_transform(df["abstract"])
     print(tfidf_vectorizer.get_feature_names())
