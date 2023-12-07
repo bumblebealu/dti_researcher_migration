c40ab264562350c4bd8de2e05d1b44a8c990018e
Ben Michael Mellon <mellonbe@moria.egr.msu.edu>
2023-12-07 10:06:10 -0500

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -145,7 +145,7 @@ if add_sidebar == "Paper Recommender":
             top_ten = cos_sim.argsort()[0][-10:]
 
             st.write(matching_index)
-            st.write(top_ten)
+            st.write(top_ten[0])
             st.write(len(df))
             # find the title of the paper with the highest cosine similarity
             matching_title = df.iloc[matching_index,4]
