30fd8c19cc040fd155a1026a63c9f30398406b42
Ben Michael Mellon <mellonbe@moria.egr.msu.edu>
2023-12-07 10:20:00 -0500

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -147,7 +147,7 @@ if add_sidebar == "Paper Recommender":
             # reverse the order of top_ten
             top_ten = top_ten[::-1]
             top_sim = cos_sim[0][top_ten]
-
+            st.write(top_sim)
             # st.write(matching_index)
             # st.write(top_ten)
             # st.write(len(df))
