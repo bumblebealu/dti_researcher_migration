1f9b9e065fa262e5b61605845f751f7a3fc51f6b
Ben Michael Mellon <mellonbe@moria.egr.msu.edu>
2023-12-10 16:57:46 -0500

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,7 +140,7 @@ if add_sidebar == "Paper Recommender":
             matching_title = df.iloc[matching_index,4]
             top_ten_titles = df.iloc[top_ten,4]
             # change the index of top_ten_titles to the similarity scores
-            top_ten_titles.["Similarity"] = top_sim
+            top_ten_titles["Similarity"] = top_sim
             st.write(matching_title)
             st.write(top_ten_titles)
             
